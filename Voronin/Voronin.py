cbfef5ec4510e99786d477669b6b570fceca924b
v0rObey <51207695+v0rObey@users.noreply.github.com>
2019-06-13 21:00:41 +0300

    Update Voronin.py

--- Voronin/Voronin.py
+++ Voronin/Voronin.py
@@ -1,2 +1,23 @@
-#test
-#ugfuhgfopipoipoijdsfgsdfg987
+def f(name=None, *args, **kwargs):
+    print(f'Hello (name)')
+
+def dec(some_func):
+    def wrapper(name=None, *args, **kwargs):
+        print('before call')
+        print(*args)
+        print(**kwargs)
+        some_func(name)
+        print('after call')
+    return wrapper
+
+decor_func = dec(f)
+print()
+
+some_name = 'Dima'
+
+@dec
+def another_func(some_name, name=None):
+    print(f'from another func (some name)')
+
+
+another_func(some_name)
